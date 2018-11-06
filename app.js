95c210b7b0c9208435f91a8ee5c270d6fb3f8c53
Lea SANCHEZ <22092@PST8556.sciences-po.fr>
2018-11-06 11:41:49 +0100

    with prompt and exercise

--- app.js
+++ app.js
@@ -1,39 +1,33 @@
 var app = new Vue({
   el: '#app',
   data: {
-    quiz: quiz, // Références aux questions du quiz
-    current: 0, // Numéro de la question en cours
-    success: null, // null si pas de réponse, true si succès, false si erreur
-    score: 0 // Incrémentation du score final
+    quiz: quiz, 
+    current: 0, 
+    success: null, 
+    score: 0 
   },
   computed: {
     step: function step () {
-      // Renvoie l'objet de la question en cours
       return this.quiz[this.current];
     }
   },
   methods: {
     submitButton: function submitButton (answer) {
-      // Modifie la propriété succès en fonction de la réponse (QCM)
       var correction = this.step.choices[this.step.answer];
-
-      // ******* Ecris ici la condition nécessaire
+      correction == answer ? this.success = true : this.success = false
     },
     submitText: function submitText () {
-      // Modifie la propriété succès en fonction de la réponse (réponse libre)
       var answer = this.$refs.textInput.value;
       var correction = this.step.answer;
-
-      // ******* Ecris ici la condition nécessaire
+      correction == answer ? this.success = true : this.success = false
     },
     nextQuestion: function nextQuestion () {
-      // Incrémente le score, réinitialise la propriété succès, passe à la question suivante
-
-      // ******* Ecris ici les actions et la condition nécessaires
+      if (this.success == true)
+        this.score++
+      this.success = null
+      this.current++
     },
     buttonClasses: function buttonClasses (value) {
-      // Renvoie les classes pour colorer les bouttons QCM en fonction de la réponse
-      // Ne pas modifier :)
       var correction = this.step.choices[this.step.answer];
       if (this.success === true) {
         if (correction === value) {
